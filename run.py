e390160bf9165a292cf567096cfce1218166e8f8
Mono The Protogen <64149624+MonoTheProtogen@users.noreply.github.com>
2025-12-16 22:36:04 +0100

    Update run and splash scripts

--- run.py
+++ run.py
@@ -5,8 +5,12 @@ import sys
 def main():
     if "--help" in sys.argv or "-h" in sys.argv:
         print("Usage: zeropda [args]\n\nArguments:\n--splash / -s: show splash screen\n--help / -h: show this message")
-    if "--splash" in sys.argv or "-s" in sys.argv:
+    elif "--splash" in sys.argv or "-s" in sys.argv:
         splash.run()
+
+def home():
+    return
+
 
 if __name__ == "__main__":
     main()
--- splash.py
+++ splash.py
@@ -7,15 +7,17 @@ logo = [
     r"|__     |.-----..----..-----.|   __ \|     \ |   _   |",
     r"|     __||  -__||   _||  _  ||    __/|  --  ||       |",
     r"|_______||_____||__|  |_____||___|   |_____/ |___|___|",                                                       
-    r"",
-    r"",
-    r" Welcome " + getpass.getuser() + "!",                                              
+    "",
+    "",
+    " Welcome " + getpass.getuser() + "!",                                              
 ]
 
 def splash(stdscr, duration=10):
     curses.curs_set(0)
     stdscr.clear()
     stdscr.nodelay(True)
+    curses.start_color()
+    curses.use_default_colors()
 
     rows, cols = stdscr.getmaxyx()
     art_height = len(logo)
@@ -23,8 +25,14 @@ def splash(stdscr, duration=10):
     start_y = (rows - art_height) // 2
     start_x = (cols - art_width) // 2
 
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
     for i, line in enumerate(logo):
-        stdscr.addstr(start_y + i, start_x, line)
+        if i > 4:
+            stdscr.addstr(start_y + i, start_x, line, curses.color_pair(2))
+        else:
+            stdscr.addstr(start_y + i, start_x, line, curses.color_pair(1))
 
     bar_width = 20
     block_width = 5
@@ -34,26 +42,31 @@ def splash(stdscr, duration=10):
 
     start_time = time.time()
 
-    bar_start = "["
-    bar_mid = "#"
-    bar_end = "]"
+    bar_start = "╺"
+    bar_mid = "╍"
+    bar_end = "╸"
 
     message = "Loading..."
     message_y = bar_y + 2
     message_x = (cols - len(message)) // 2
 
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
     while time.time() - start_time < duration:
-        stdscr.addstr(bar_y, bar_x, " " * bar_width)
-        stdscr.addstr(bar_y, bar_x - 1, bar_start) 
-        stdscr.addstr(bar_y, bar_x + bar_width, bar_end) 
+        stdscr.addstr(bar_y, bar_x, " " * bar_width, curses.color_pair(3))
+        stdscr.addstr(bar_y, bar_x - 1, bar_start, curses.color_pair(3)) 
+        stdscr.addstr(bar_y, bar_x + bar_width, bar_end, curses.color_pair(3)) 
 
         if(time.time() - start_time > 7):
             message = "All done! "
+            stdscr.addstr(message_y, message_x, message, curses.color_pair(2))
+        else:
+            stdscr.addstr(message_y, message_x, message, curses.color_pair(1))
 
-        stdscr.addstr(message_y, message_x, message)
+        
 
         for i in range(block_width):
-            stdscr.addstr(bar_y, bar_x + (pos + i) % bar_width, bar_mid)
+            stdscr.addstr(bar_y, bar_x + (pos + i) % bar_width, bar_mid, curses.color_pair(3))
 
         stdscr.refresh()
         stdscr.border()
